353143b82f9310bbc7c2a8e287f11d7905f751c8
Karl Tayeb <karl.tayeb7@gmail.com>
2016-04-17 14:36:28 -0400

    made WEdge and GVertex generic

---
+++ GVertex.java
@@ -0,0 +1,62 @@
+/** Class to represent a vertex (in a graph).
+ */
+public class GVertex<T extends Comparable<T>> implements Comparable<GVertex<T>> {
+
+    /* Note that the nextID variable had to be moved to the graph class. */
+
+    /** Vertex unique ID number. */
+    private int num;
+
+    /** Data stored in the vertex. */
+    private T data;
+
+    /** Create a new vertex.
+     *  @param d the data to store in the node
+     *  @param id the unique id of the node
+     */
+    public GVertex(T d, int id) {
+        this.data = d;
+        this.num = id;
+    }
+
+    /** Get the id of this vertex.
+     *  @return the id
+     */
+    public int id() {
+        return this.num;
+    }
+
+    /** Get a string representation of the vertex.
+     *  @return the string 
+     */
+    public String toString() {
+        return this.num + "";
+    }
+
+    /** Check if two vertices are the same based on ID.
+     *  @param other the vertex to compare to this
+     *  @return true if the same, false otherwise
+     */
+    public boolean equals(Object other) {
+        if (other instanceof GVertex) {
+            GVertex<T> v = (GVertex<T>) other;
+            return this.num == v.num;  // want these to be unique
+        }
+        return false;
+    }
+
+    /** Get the hashcode of a vertex based on its ID.
+     *  @return the hashcode
+     */
+    public int hashCode() {
+        return (new Integer(this.num)).hashCode();
+    }
+
+    /** Compare two vertices based on their IDs.
+     *  @param other the vertex to compare to this
+     *  @return negative if this < other, 0 if equal, positive if this > other
+     */
+    public int compareTo(GVertex<T> other) {
+        return this.num - other.num;
+    }
+}
---
+++ WEdge.java
@@ -0,0 +1,98 @@
+/** Implementation of an edge class (for graphs), could be directed or not.
+ */
+public class WEdge<VT extends Comparable<VT>> implements Comparable<WEdge<VT>> {
+
+    /** Starting vertex of an edge. */
+    private GVertex<VT> source;
+    /** Ending vertex of an edge. */
+    private GVertex<VT> end;
+    /** Weight of the edge */
+    private double weight;
+
+    /** Create an ndirected edge.
+     *  @param u the start
+     *  @param v the end
+     */
+
+    // How do we initialize a default generic value N for weight?
+    public WEdge(GVertex<VT> u, GVertex<VT> v) {
+        this.source = u;
+        this.end = v;
+        this.weight = 0; // initialize weight to 0
+    }
+
+    /** Create an edge.
+     *  @param u the start
+     *  @param v the end
+     *  @param dir true if directed, false otherwise
+     */
+    public WEdge(GVertex<VT> u, GVertex<VT> v, double w) {
+        this.source = u;
+        this.end = v;
+        this.weight = w;
+    }
+
+    /** Is a vertex incident to this edge.
+     *  @param v the vertex
+     *  @return true if source or end, false otherwise
+     */
+    public boolean isIncident(GVertex<VT> v) {
+        return this.source.equals(v) || this.end.equals(v);
+    }
+
+    /** Get the starting endpoint vertex.
+     *  @return the vertex
+     */
+    public GVertex<VT> source() {
+        return this.source;
+    }
+
+    /** Get the ending endpoint vertex.
+     *  @return the vertex
+     */
+    public GVertex<VT> end() {
+        return this.end;
+    }
+
+    /** Create a string representation of the edge.
+     *  @return the string as (source,end)
+     */
+    public String toString() {
+        return "(" + this.source + "," + this.end + "," + this.weight + ")";
+    }
+
+    /** Check if two edges are the same.
+     *  @param other the edge to compare to this
+     *  @return true if directedness and endpoints match, false otherwise
+     */
+    public boolean equals(Object other) {
+        if (other instanceof WEdge) {
+            WEdge<VT> e = (WEdge<VT>) other;
+            return this.source.equals(e.source)
+                && this.end.equals(e.end)
+                || this.source.equals(e.end)
+                && this.end.equals(e.source);
+        }
+        return false;
+    }
+
+    /** Make a hashCode based on the toString.
+     *  @return the hashCode
+     */
+    public int hashCode() {
+        return this.toString().hashCode();
+    }
+
+
+    @Override
+    public int compareTo(WEdge<VT> other) {
+        // not sure if this is good
+        if (this.weight > other.weight) {
+            return 1;
+        } else if (this.weight < other.weight) {
+            return -1;
+        }
+        return 0;
+    }
+
+}
